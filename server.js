b881610ac5677cf3820c3dd28caec9be11d4eb39
tsrhayar <>
2021-05-18 16:56:38 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import data from "./data.js";
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) res.send(product);
+  else res.status(404).send({ message: "Product Not Found" });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
