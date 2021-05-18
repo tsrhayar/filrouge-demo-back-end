199ae3c0c11b8b300797bfff949b15a9e7caaf1e
tsrhayar <>
2021-05-18 17:58:33 +0100

    create user model && database

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
@@ -11,6 +20,12 @@ app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) res.send(product);
   else res.status(404).send({ message: "Product Not Found" });
+});
+
+app.use("/api/users", userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 app.get("/", (req, res) => {
