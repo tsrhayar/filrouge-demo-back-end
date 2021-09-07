63a4d3bcf049c139955faeb7463521470009bdd2
tsrhayar <>
2021-09-07 21:23:54 +0100

    commit

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
 
 import data from "../data.js";
 import User from "../models/userModel.js";
+import { generateToken } from "../utils.js";
 
 const userRouter = express.Router();
 
@@ -12,6 +14,51 @@ userRouter.get(
     await User.remove({});
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
+  })
+);
+
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+          test:
+          {
+            id2: user._id,
+            name2: user.name
+          }
+
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Email ou mot de passe invalide" });
+  })
+);
+
+userRouter.post(
+  '/registre',
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
+    });
   })
 );
 
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from "express";
-import data from "./data.js";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
+import orderRouter from "./routers/orderRouter.js";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useNewUrlParser: true,
@@ -12,17 +19,12 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useCreateIndex: true,
 });
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) res.send(product);
-  else res.status(404).send({ message: "Product Not Found" });
-});
-
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+}); 
+app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
