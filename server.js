fda09832534abdb34c0fe76bf50f74d6cde39b1c
tsrhayar <>
2021-09-14 15:45:26 +0100

    manage product

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import path from 'path';
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
 import orderRouter from "./routers/orderRouter.js";
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 
@@ -19,20 +20,23 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useCreateIndex: true,
 });
 
-app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-}); 
+app.use('/api/uploads', uploadRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use('/api/orders', orderRouter);
-
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
